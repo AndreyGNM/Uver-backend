da74696f4a7edf4128a95f39e33cd70bc6414304
jose vargas <jose.vargaslopez@ucr.ac.cr>
2024-06-15 19:40:04 -0600

    añadidos metodos al controlador principal, creado el controlador de Twilio y añadidos métodos vacíos solicitados en el diseño de responsabilidades

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
 
-Route::post('/usuarios', [ApiController::class, 'crearUsuario']);
-Route::post('/viajes', [ApiController::class, 'crearViaje']);
+Route::post('/usuarios', [ApiController::class, 'registerUser']);
+Route::post('/viajes', [ApiController::class, 'createTravel']);
